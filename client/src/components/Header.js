3e4443b7ade730ba0430bea51adef7cbbb42e06c
will-gebbie <will@gebbie.nu>
2019-06-02 13:50:03 -0700

    change brand

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
   render(){
     return(
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Tasks</Navbar.Brand>
+        <Navbar.Brand href="/">MERN Tasks</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
